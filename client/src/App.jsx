479ec0bca8442885981dc53c8c0314edfc157793
Ankittt81 <121846044+Ankittt81@users.noreply.github.com>
2025-09-23 01:20:02 +0530

    setup

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+
+function App() {
+  return (
+    <>
+    <Button className="bg-blue-600">Check</Button>
+    </>
+  )
+}
+
+export default App
---
+++ server/server.js
@@ -0,0 +1,26 @@
+require("dotenv").config(); // Load environment variables from .env file
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const MONGO_URL = process.env.MONGO_URL;
+
+cors({
+  origin: process.env.CLIENT_URL,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Contenet-Type", "Authorization"],
+});
+
+app.use(express.json());
+
+//database connection
+mongoose
+  .connect(MONGO_URL)
+  .then(() => console.log("Database connected"))
+  .catch((err) => console.log(err));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
